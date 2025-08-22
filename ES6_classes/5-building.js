b83ca4162cc4f657258a0654e1bbf9d755f0a79c
Hussah ✨ <10685@holbertonstudents.com>
2025-08-22 15:08:54 +0300

    Update 5-building.js

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,16 +1,20 @@
-// 5-building.js
 export default class Building {
-    constructor(sqft) {
-      this._sqft = sqft;
-  
-      // Check if the extending class implements evacuationWarningMessage
-      if (new.target !== Building && typeof this.evacuationWarningMessage !== 'function') {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-      }
-    }
-  
-    // Getter for sqft
-    get sqft() {
-      return this._sqft;
+  constructor(sqft) {
+    this._sqft = sqft;
+
+    // Check if the subclass overrides evacuationWarningMessage
+    // If this method is exactly the same as the one on Building's prototype, throw an error
+    if (this.constructor !== Building && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error("Class extending Building must override evacuationWarningMessage");
     }
   }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    // This method should be overridden by subclasses
+    throw new Error("Class extending Building must override evacuationWarningMessage");
+  }
+}
