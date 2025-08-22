5b7bcef09b1078e4e522c902f65c6e5cc63dff51
Hussah ✨ <10685@holbertonstudents.com>
2025-08-22 15:12:34 +0300

    Update 2-hbtn_course.js

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,48 +1,52 @@
 class HolbertonCourse {
-  constructor(name, length, students) {
-    this.name = name;
-    this.length = length;
-    this.students = students;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  set name(name) {
-    if (typeof name === 'string') {
+    constructor(name, length, students) {
+      this.name = name;
+      this.length = length;
+      this.students = students;
+    }
+  
+    /**
+     * @param {String} name
+     */
+    set name(name) {
+      if (typeof name !== 'string') {
+        throw new TypeError('Name must be a string');
+      }
       this._name = name;
-    } else {
-      throw new TypeError('Name must be a string');
     }
-  }
-
-  get length() {
-    return this._length;
-  }
-
-  set length(length) {
-    if (typeof length === 'number') {
+  
+    get name() {
+      return this._name;
+    }
+  
+    /**
+     * @param {Number} length
+     */
+    set length(length) {
+      if (typeof length !== 'number') {
+        throw new TypeError('Length must be a number');
+      }
       this._length = length;
-    } else {
-      throw new TypeError('Length must be a number');
+    }
+  
+    get length() {
+      return this._length;
+    }
+  
+    /**
+     * @param {Array} students
+     */
+    set students(students) {
+      if (students instanceof Array) {
+        this._students = students;
+      } else {
+        throw new TypeError('Students must be an Array');
+      }
+    }
+  
+    get students() {
+      return this._students;
     }
   }
-
-  get students() {
-    return this._students;
-  }
-
-  set students(students) {
-    if (
-      Array.isArray(students)
-      && students.every((s) => typeof s === 'string')
-    ) {
-      this._students = students;
-    } else {
-      throw new TypeError('Students must be an array of strings');
-    }
-  }
-}
-
-export default HolbertonCourse;
+  
+  export default HolbertonCourse;
