8702153ce7cf2e6bc54885c751ce9b2f41ab3238
Hussah ✨ <10685@holbertonstudents.com>
2025-08-28 02:32:44 +0300

    Implement handleProfileSignup to manage user signup

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUp = signUpUser(firstName, lastName);
+  const upload = uploadPhoto(fileName);
+
+  return Promise.allSettled([upload, signUp]).then(results =>
+    results.map(res => ({
+      status: res.status,
+      value: res.status === 'fulfilled' ? res.value : res.reason,
+    }))
+  );
+}
