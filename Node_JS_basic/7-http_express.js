f7b9796066c3e6642c5da6692b08131ad000e903
Hussah ✨ <10685@holbertonstudents.com>
2025-09-06 13:02:50 +0300

    Refactor student retrieval and error handling

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,23 +1,29 @@
 const express = require('express');
+
+const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
+const DATABASE = args[0];
+
 const app = express();
-const dbFile = process.argv[2];
+const port = 1245;
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
+  const msg = 'This is the list of our students\n';
   try {
-    const chunks = ['This is the list of our students'];
-    await countStudents(dbFile);
-    res.send(chunks.join('\n'));
-  } catch (err) {
-    res.status(500).send(err.message);
+    const students = await countStudents(DATABASE);
+    res.send(`${msg}${students.join('\n')}`);
+  } catch (error) {
+    res.send(`${msg}${error.message}`);
   }
 });
 
-app.listen(1245);
+app.listen(port, () => {
+  //   console.log(`Example app listening at http://localhost:${port}`);
+});
 
 module.exports = app;
